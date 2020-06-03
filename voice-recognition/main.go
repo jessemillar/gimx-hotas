b0ae82b1de6958ea4bd634ed5495bcc1816000dc
Jesse Millar <jesse.millar@microsoft.com>
2020-06-03 15:05:52 -0500

    Tweaking available voice commands
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- voice-recognition/main.go
+++ voice-recognition/main.go
@@ -14,13 +14,19 @@ import (
 )
 
 var keywordConfig = map[string]int{
-	"launch elite dangerous": keybd_event.VK_Z,
-	"landing gear":           keybd_event.VK_X,
-	"docking permissions":    keybd_event.VK_C,
-	"super cruise":           keybd_event.VK_V,
-	"supercruise":            keybd_event.VK_V,
-	"take us up":             keybd_event.VK_B,
-	"warp drive":             keybd_event.VK_N,
+	// TODO Fix the hotkeys for these in the macro file
+	// "landing gear":           keybd_event.VK_X,
+	// "docking permissions":    keybd_event.VK_C,
+	// "super cruise":           keybd_event.VK_V,
+	// "supercruise":            keybd_event.VK_V,
+	// "take us up":             keybd_event.VK_B,
+	// "warp drive":             keybd_event.VK_N,
+	// "communications panel":   keybd_event.VK_W,
+	// "comms panel":            keybd_event.VK_W,
+	"ship panel":       keybd_event.VK_D,
+	"control panel":    keybd_event.VK_S,
+	"navigation panel": keybd_event.VK_A,
+	"nav panel":        keybd_event.VK_A,
 }
 
 var keyboard keybd_event.KeyBonding
