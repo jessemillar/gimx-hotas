8f17495aa543aed621a5d562f4c11b8442f45eaa
Jesse Millar <jesse.millar@microsoft.com>
2020-06-03 23:58:36 -0500

    Improving doc generation by removing inline comments
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- generate-mapping-reference.sh
+++ generate-mapping-reference.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ALL_MACRO_COMMENTS=$(grep "^#" < gimx/.gimx/macros/vkb-gladiator-mk-ii.txt)
+ALL_MACRO_COMMENTS=$(grep "^##" < gimx/.gimx/macros/vkb-gladiator-mk-ii.txt)
 
 echo "$ALL_MACRO_COMMENTS" > mapping-reference.txt
