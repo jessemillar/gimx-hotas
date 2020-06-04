50285d7b110fcbc55b0b7183153e11d90f9666d5
Jesse Millar <jesse.millar@microsoft.com>
2020-06-04 17:05:45 -0500

    Trying to get permissions working for Bluetooth and Azure Voice
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- start-voice-recognition.sh
+++ start-voice-recognition.sh
@@ -2,5 +2,14 @@
 
 (
 cd voice-recognition || return
-sudo chmod +0666 /dev/uinput && go install && go run main.go
+sudo chmod +0666 /dev/uinput
+go build main.go
+
+# Configure the Azure Speech API
+SPEECHSDK_ROOT="$HOME/.speechsdk"
+CGO_CFLAGS="-I$SPEECHSDK_ROOT/include/c_api"
+CGO_LDFLAGS="-L$SPEECHSDK_ROOT/lib/arm32 -lMicrosoft.CognitiveServices.Speech.core"
+LD_LIBRARY_PATH="$SPEECHSDK_ROOT/lib/arm32:$LD_LIBRARY_PATH"
+
+sudo SPEECHSDK_ROOT="$SPEECHSDK_ROOT" CGO_CFLAGS="$CGO_CFLAGS" CGO_LDFLAGS="$CGO_LDFLAGS" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ./main
 )
